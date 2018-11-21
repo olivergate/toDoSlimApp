72e54f482ef591b718eea4951ca74f58a38931f1
olivergate <oliver.kg2@gmail.com>
2018-11-21 13:24:44 +0000

    initial Classes commit

---
+++ src/Classes/Factories/todoModelFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 21/11/2018
+ * Time: 13:23
+ */
+
+namespace Classes\Factories;
+
+
+class todoModelFactory
+{
+
+}
---
+++ src/Classes/Models/todoModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 21/11/2018
+ * Time: 13:24
+ */
+
+namespace Classes\Models;
+
+
+class todoModel
+{
+
+}
